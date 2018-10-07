1f1a8887a1e680a881795767ce5aa1b2644a5a8f
yoshi <yasharsabaz@gmail.com>
2018-10-07 23:43:55 +0300

    Added functionality for downloading players info and reading the data.

--- automatic_coach_selenium.py
+++ automatic_coach_selenium.py
@@ -17,11 +17,21 @@ from selenium.webdriver.common.keys import Keys
 from read_function import * 
 from action_functions import *
 
+
+# For downloading automatically
+profile = webdriver.FirefoxProfile()
+profile.set_preference('browser.download.folderList', 2) # custom location
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.download.dir', '/home/haidin/Desktop/Test/first_test')
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/csv,text/csv')
+
+
 # Create driver and give url address
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(profile)
 url= 'https://rockingsoccer.com/en/soccer'
 driver.get(url)
 assert "Rocking Soccer" in driver.title
+
 
 
 # Log into the game
@@ -54,7 +64,25 @@ finances_elem.click()
 balance_sheet_pd_table, account_table_pd = obtain_finance_tables(driver)
 
 
+####################### Read players' information
+time.sleep(3)
+finances_elem = driver.find_element_by_id('menu-entry-players')
+finances_elem.click()
+
+# players_data = download_and_read_players_data(driver)
+time.sleep(3)
+cookie_accept_button = driver.find_element_by_css_selector('.cc-btn')
+cookie_accept_button.click()
     
+time.sleep(2)
+export_to_csv = driver.find_elements_by_class_name('submit')
+export_to_csv = export_to_csv[2]
+export_to_csv.click()
+
+# Read csv file
+file_name = time.strftime('%Y_%m_%d') + '_players.csv'
+players_data = pd.read_csv(file_name, sep=';')
+
 
 # Logout and shutdown web browser
 logout(driver)
